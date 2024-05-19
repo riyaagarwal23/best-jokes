161913b486568f9eb2e7dfb6ce08240b5f9858b4
riyaagarwal23 <74665478+riyaagarwal23@users.noreply.github.com>
2024-05-19 20:28:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// GET https://icanhazdadjoke.com/
+
+const jokes= document.querySelector('#joke');
+const jokeBtn= document.querySelector('#jokeBtn');
+
+// Promise method
+// const generateJokes = () => {
+//     const setHeader= {
+//         headers: {
+//             Accept: "application/json"
+//         }
+//     }
+//     fetch('https://icanhazdadjoke.com/', setHeader).then((res) => res.json() ).then((data) => {
+//         jokes.innerHTML= data.joke;
+//     }).catch((error) => {
+//         console.log(error);
+//     })
+// }
+// jokeBtn.addEventListener('click', generateJokes);
+// generateJokes();
+
+
+// asyn-await
+const generateJokes = async () => {
+    try{
+        const setHeader= {
+            headers: {
+                Accept: "application/json"
+            }
+        }
+        const res = await fetch('https://icanhazdadjoke.com/', setHeader);
+        const data = await res.json();
+        jokes.innerHTML = data.joke;
+    }
+    catch(err){
+        console.log(`the error is ${err}`);
+    }
+}
+jokeBtn.addEventListener('click', generateJokes);
+generateJokes();
